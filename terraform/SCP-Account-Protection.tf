2a46e0bd4ef2c473f24c1c40be87410a5c6d8420
George Rolston <grolston@users.noreply.github.com>
2021-07-29 14:38:57 -0600

    Create SCP-Account-Protection.tf

---
+++ terraform/SCP-Account-Protection.tf
@@ -0,0 +1,97 @@
+provider "aws" {
+}
+
+## Requires to enter regions allowed to be used starting line 59.
+resource "aws_organizations_policy" "Scp_account_protection" {
+  name = "account_protection"
+  description = "account_protection"
+  content = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "NotAction": [
+        "a4b:*",
+        "acm:*",
+        "aws-marketplace-management:*",
+        "aws-marketplace:*",
+        "aws-portal:*",
+        "awsbillingconsole:*",
+        "budgets:*",
+        "ce:*",
+        "chime:*",
+        "cloudfront:*",
+        "config:*",
+        "cur:*",
+        "directconnect:*",
+        "ec2:DescribeRegions",
+        "ec2:DescribeTransitGateways",
+        "ec2:DescribeVpnGateways",
+        "fms:*",
+        "globalaccelerator:*",
+        "health:*",
+        "iam:*",
+        "importexport:*",
+        "kms:*",
+        "mobileanalytics:*",
+        "networkmanager:*",
+        "organizations:*",
+        "pricing:*",
+        "route53:*",
+        "route53domains:*",
+        "s3:GetAccountPublic*",
+        "s3:ListAllMyBuckets",
+        "s3:PutAccountPublic*",
+        "shield:*",
+        "sts:*",
+        "support:*",
+        "trustedadvisor:*",
+        "waf-regional:*",
+        "waf:*",
+        "wafv2:*",
+        "wellarchitected:*"
+      ],
+      "Resource": "*",
+      "Effect": "Deny",
+      "Condition": {
+        "StringNotEquals": {
+          "aws:RequestedRegion": [
+            ""
+          ]
+        }
+      }
+    },
+    {
+      "Action": "*",
+      "Resource": "*",
+      "Effect": "Deny",
+      "Condition": {
+        "StringLike": {
+          "aws:PrincipalArn": [
+            "arn:aws:iam::*:root"
+          ]
+        }
+      }
+    },
+    {
+      "Action": [
+        "organizations:LeaveOrganization"
+      ],
+      "Resource": "*",
+      "Effect": "Deny"
+    },
+    {
+      "Action": [
+        "aws-portal:ModifyAccount",
+        "aws-portal:ModifyBilling",
+        "aws-portal:ModifyPaymentMethods"
+      ],
+      "Resource": "*",
+      "Effect": "Deny"
+    }
+  ]
+}
+POLICY
+
+}
+
